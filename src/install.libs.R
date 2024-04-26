a34559f7f6d719a62ed3124a7e4b39aa32f289a3
Matthias Gondan <Matthias.Gondan-Rochon@uibk.ac.at>
2024-04-26 12:21:58 +0200

    symbolic links for libswipl.so

---
+++ src/install.libs.R
@@ -0,0 +1,16 @@
+files <- Sys.glob(paste0("*", SHLIB_EXT))
+dest <- file.path(R_PACKAGE_DIR, paste0("libs", R_ARCH))
+dir.create(dest, recursive=TRUE, showWarnings=FALSE)
+file.copy(files, dest, overwrite=TRUE)
+if(file.exists("symbols.rds"))
+    file.copy("symbols.rds", dest, overwrite=TRUE)
+
+# The below is needed because file.copy does not copy symbolic links
+if(SHLIB_EXT == ".so")
+{
+  unlink(file.path("inst", "swipl", "lib", "libswipl.so"))
+  unlink(file.path("inst", "swipl", "lib", "libswipl.so.9"))
+  dir.create(file.path(R_PACKAGE_DIR, "swipl", "lib"), recursive=TRUE, showWarnings=FALSE)
+  file.symlink("libswipl.so.9", file.path(R_PACKAGE_DIR, "swipl", "lib", "libswipl.so"))
+  file.symlink("libswipl.so.9.3.3", file.path(R_PACKAGE_DIR, "swipl", "lib", "libswipl.so.9"))
+}
