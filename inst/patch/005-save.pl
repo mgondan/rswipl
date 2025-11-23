b5cea75769b4d003446b71399ea0af7c1ecbd98b
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2025-11-23 17:56:29 +0100

    skip stream timeout on windows

---
+++ inst/patch/005-save.pl
@@ -0,0 +1,14 @@
+--- swipl-devel/tests/save/test_saved_states.pl	2025-11-23 17:54:03.256571446 +0100
++++ swipl-devel/tests/save/test_saved_states.pl.new	2025-11-23 17:52:38.075100230 +0100
+@@ -202,7 +202,10 @@
+ %   saved state does not terminate.
+ 
+ read_terms(In, List) :-
+-    set_stream(In, timeout(60)),
++    (   current_prolog_flag(windows, true)
++    ->  true
++    ;   set_stream(In, timeout(60))
++    ),
+     read_term(In, T0, []),
+     read_terms(T0, In, List).
+ 
--- inst/patch/patches.sh
+++ inst/patch/patches.sh
@@ -3,3 +3,5 @@ cd ../../src
 patch -p2 < ../inst/patch/02-clib.patch
 patch -p2 < ../inst/patch/03-cpp.patch
 patch -p2 < ../inst/patch/04-cpp.patch
+patch -p2 < ../inst/patch/05-save.patch
+
