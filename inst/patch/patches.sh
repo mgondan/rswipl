b50a412f1f4dbeb7335cf096b3748fba96d1423d
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2025-12-19 22:00:59 +0100

    fix an NULL offset problem reported by clang-21 asan

--- inst/patch/patches.sh
+++ inst/patch/patches.sh
@@ -8,3 +8,5 @@ patch -p2 < ../inst/patch/02-clib.patch
 patch -p2 < ../inst/patch/03-cpp.patch
 patch -p2 < ../inst/patch/04-cpp.patch
 patch -p2 < ../inst/patch/05-variant.patch
+patch -p2 < ../inst/patch/06-incl.patch
+patch -p2 < ../inst/patch/06-cont.patch
