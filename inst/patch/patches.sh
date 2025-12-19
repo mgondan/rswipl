cc1c1279728585f1f0b7cdc127175cd8d0195fd5
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2025-12-19 18:52:21 +0100

    fixes a clang-21 UBSAN issue

--- inst/patch/patches.sh
+++ inst/patch/patches.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-cd ../../src 
+cd ../../src
+patch -p2 < ../inst/patch/01-clib.patch
+patch -p2 < ../inst/patch/01-cpp.patch
+patch -p2 < ../inst/patch/01-swi.patch
+patch -p2 < ../inst/patch/01-thread.patch
 patch -p2 < ../inst/patch/02-clib.patch
 patch -p2 < ../inst/patch/03-cpp.patch
 patch -p2 < ../inst/patch/04-cpp.patch
