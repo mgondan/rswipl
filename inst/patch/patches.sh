0cc0d633cb8bcaee891dfffb2dc4b444d4146b30
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2025-12-19 20:57:47 +0100

    fix a null pointer offset reported by clang-21 asan

--- inst/patch/patches.sh
+++ inst/patch/patches.sh
@@ -7,4 +7,4 @@ patch -p2 < ../inst/patch/01-thread.patch
 patch -p2 < ../inst/patch/02-clib.patch
 patch -p2 < ../inst/patch/03-cpp.patch
 patch -p2 < ../inst/patch/04-cpp.patch
-
+patch -p2 < ../inst/patch/05-variant.patch
