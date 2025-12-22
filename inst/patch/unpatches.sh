a51f47ca9f09dd78dde48012378cf64c3bceeb22
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2025-12-22 19:21:47 +0100

    Update unpatches.sh

--- inst/patch/unpatches.sh
+++ inst/patch/unpatches.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 cd ../../src
-patch -p2 -R < ../inst/patch/01-clib.patch
-patch -p2 -R < ../inst/patch/01-cpp.patch
-patch -p2 -R < ../inst/patch/01-swi.patch
-patch -p2 -R < ../inst/patch/01-thread.patch
 patch -p2 -R < ../inst/patch/02-clib.patch
 patch -p2 -R < ../inst/patch/03-cpp.patch
 patch -p2 -R < ../inst/patch/04-cpp.patch
-patch -p2 -R < ../inst/patch/05-variant.patch
-patch -p2 -R < ../inst/patch/06-incl.patch
-patch -p2 -R < ../inst/patch/06-cont.patch
