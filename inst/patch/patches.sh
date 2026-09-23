dcea93872c8c02b60899a33782c629af1d22385f
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2026-09-24 00:02:15 +0200

    robust w.r.t. 8.3 filenames in test-qlf

--- inst/patch/patches.sh
+++ inst/patch/patches.sh
@@ -4,3 +4,4 @@ patch -p2 < ../inst/patch/02-clib.patch
 patch -p2 < ../inst/patch/03-cpp.patch
 patch -p2 < ../inst/patch/04-cpp.patch
 patch -p2 < ../inst/patch/05-test-load-context.patch
+patch -p2 < ../inst/patch/06-test-qlf.patch
--- inst/patch/unpatches.sh
+++ inst/patch/unpatches.sh
@@ -4,3 +4,4 @@ patch -p2 -R < ../inst/patch/02-clib.patch
 patch -p2 -R < ../inst/patch/03-cpp.patch
 patch -p2 -R < ../inst/patch/04-cpp.patch
 patch -p2 -R < ../inst/patch/05-test-load-context.patch
+patch -p2 -R < ../inst/patch/06-patch-qlf.patch
