8d84c6b41ba19f601c6cc5842c38924c87a5f885
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2026-09-23 22:50:06 +0200

    test robust w.r.t. 8.3 filenames

--- inst/patch/unpatches.sh
+++ inst/patch/unpatches.sh
@@ -3,3 +3,4 @@ cd ../../src
 patch -p2 -R < ../inst/patch/02-clib.patch
 patch -p2 -R < ../inst/patch/03-cpp.patch
 patch -p2 -R < ../inst/patch/04-cpp.patch
+patch -p2 -R < ../inst/patch/05-test-load-context.patch
