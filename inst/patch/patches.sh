0d0a087730303b06b244c07a0741a1d6aeb18368
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2026-09-24 15:19:47 +0200

    update to head and deactivate patches

--- inst/patch/patches.sh
+++ inst/patch/patches.sh
@@ -3,6 +3,6 @@ cd ../../src
 patch -p2 < ../inst/patch/02-clib.patch
 patch -p2 < ../inst/patch/03-cpp.patch
 patch -p2 < ../inst/patch/04-cpp.patch
-patch -p2 < ../inst/patch/05-test-load-context.patch
-patch -p2 < ../inst/patch/06-test-qlf.patch
-patch -p2 < ../inst/patch/07-init.patch
+# patch -p2 < ../inst/patch/05-test-load-context.patch
+# patch -p2 < ../inst/patch/06-test-qlf.patch
+# patch -p2 < ../inst/patch/07-init.patch
